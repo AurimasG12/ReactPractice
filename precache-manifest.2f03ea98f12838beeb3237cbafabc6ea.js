dadc37af2a41e319bc6984a16b8d333622080d70
AurimasG12 <aurimas.gel1@gmail.com>
2020-09-22 13:47:00 +0300

    Updates

---
+++ precache-manifest.2f03ea98f12838beeb3237cbafabc6ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d033557ec73e76bbb81e2b4a50bc6b",
+    "url": "/ReactPractice/index.html"
+  },
+  {
+    "revision": "c61a1ed1e9c8495706e8",
+    "url": "/ReactPractice/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "7f1282c6e073c3c11088",
+    "url": "/ReactPractice/static/css/main.1125d8b1.chunk.css"
+  },
+  {
+    "revision": "c61a1ed1e9c8495706e8",
+    "url": "/ReactPractice/static/js/2.9a1165ab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ReactPractice/static/js/2.9a1165ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f1282c6e073c3c11088",
+    "url": "/ReactPractice/static/js/main.c39199c5.chunk.js"
+  },
+  {
+    "revision": "8bc4b62af3ec346dde98",
+    "url": "/ReactPractice/static/js/runtime-main.ab2603cf.js"
+  }
+]);
